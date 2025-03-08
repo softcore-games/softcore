3fd8631710cdc1e88958b61c6f9f57f37a0cc11c
Developla <mail.technical77@gmail.com>
2025-03-08 22:42:23 +0800

    bg image scene on scene

--- app/game/[characterId]/page.tsx
+++ app/game/[characterId]/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Scene from "@/components/scene/scene";
 import ProtectedRoute from "@/components/auth/protected-route";
-import { SceneProvider } from "@/lib/contexts/SceneContext";
 
 const Page = () => {
   return (
     <ProtectedRoute>
-      <SceneProvider>
-        <Scene />
-      </SceneProvider>
+      <Scene />
     </ProtectedRoute>
   );
 };
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,41 @@
+"use client";
 import React from "react";
-
+import { SceneProvider, useScene } from "@/lib/contexts/SceneContext";
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const LayoutRender = ({ children }: LayoutProps) => {
+  const { currentScene } = useScene();
+
   return (
-    <div className="flex justify-center items-center h-[150vh] bg-white">
+    <div className="flex justify-center items-center h-[150vh] bg-black">
       <div
-        className="w-screen h-full bg-cover bg-center border-8 border-black rounded-2xl shadow-lg relative"
+        className="w-screen h-full bg-cover bg-center border-8 border-black rounded-2xl shadow-lg relative z-10"
         style={{
           backgroundColor: "#0000006e",
-          // backgroundImage: "url('/images/view.png')",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundImage: currentScene
+            ? `url('${currentScene?.imageUrl}')`
+            : "url('/images/view.png')",
         }}
       >
+        {/* Dark overlay for background image */}
+        <div className="absolute inset-0 bg-black/50 rounded-xl" />
+
         {/* Content Container */}
-        <div className="pt-20">{children}</div>
+        <div className="pt-20 relative z-10">{children}</div>
       </div>
     </div>
+  );
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <SceneProvider>
+      <LayoutRender>{children}</LayoutRender>
+    </SceneProvider>
   );
 };
 
