41c29b67b807ae410f899e09e79e0be038b1d156
Jimmy <jimmytombado99@gmail.com>
2025-03-08 15:28:38 +0800

    change the stamina to NFT Gallery

--- components/nav-bar/nav-links.tsx
+++ components/nav-bar/nav-links.tsx
@@ -12,11 +12,12 @@ export const NavLinks = ({
   className = "",
 }: NavLinksProps) => (
   <div className={`space-x-14 z-10 ${className}`}>
-    {isLoggedIn && (
+    {/* {isLoggedIn && (
       <a className="text-white text-lg hover:text-pink-400 transition-colors">
-        <span className="font-bold">{stamina}</span> STAMINA
+        <span className="font-bold">{stamina}</span> NFT Gallery
       </a>
-    )}
+    )} */}
+    <NavLink href="/nft-gallery">NFT Gallery</NavLink>
     <NavLink href="#">FAQ</NavLink>
     <NavLink href="#">{isLoggedIn ? "ABOUT" : "About"}</NavLink>
     <NavLink href="/contact">{isLoggedIn ? "CONTACT" : "Contact"}</NavLink>
